ecea6702892eaa0a2879164da4baff4570ba900b
pganda78 <pganda@gmail.com>
2026-05-07 17:13:50 -0400

    Task 3: Format the output of the application

---
+++ emotion_detection.py
@@ -0,0 +1,26 @@
+""" Module to test the Emotion Analysis of a text """
+# Import the requests library to handle HTTP requests
+import requests
+import json
+
+# Define a function named emotion_detector that takes a string input (text_to_analyze)
+def emotion_detector(text_to_analyze):
+    """ Sentiment analyzer """
+    # URL of the emotion detector service
+    url_str = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create a dictionary with the text to be analyzed
+    input_obj = { "raw_document": { "text": text_to_analyze } }
+
+    # Set the headers required for the API request
+    headers_obj = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
+
+    # Send a POST request to the API with the text and headers
+    response = requests.post(url_str, json = input_obj, headers=headers_obj, timeout=10)
+
+    # Extract the response text
+    formatted_response = json.loads(response.text)
+    emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+    emotion_dict['dominant_emotion'] = max(emotion_dict, key=emotion_dict.get)
+
+    return emotion_dict
